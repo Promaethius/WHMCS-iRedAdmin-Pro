ddd1fb339e1d9283e5d26d2b32915e79193fde97
Promaethius <anon.clipsy.42@gmail.com>
2017-09-08 16:05:56 -0700

    Update WHMCS-iRedAdmin-Pro.php
    mislabled ConfigOpptions

--- modules/servers/WHMCS-iRedAdmin-Pro/WHMCS-iRedAdmin-Pro.php
+++ modules/servers/WHMCS-iRedAdmin-Pro/WHMCS-iRedAdmin-Pro.php
@@ -15,18 +15,18 @@
 
 function WHMCS-iRedAdmin-Pro_ConfigOptions() {
     return [
-        "username" => [
+        "userprorate" => [
             "FriendlyName" => "Per-User Prorate 24H",
             "Type" => "text", # Text Box
             "Size" => "5", # Defines the Field Width
-            "Description" => "userprorate",
+            "Description" => "Cost per User charged to a WHMCS user at the end of every 24H period.",
             "Default" => "0.0",
         ],
-        "password" => [
+        "mbprorate" => [
             "FriendlyName" => "Per-MB Prorate 24H",
             "Type" => "text", # Password Field
             "Size" => "5", # Defines the Field Width
-            "Description" => "mbprorate",
+            "Description" => "Cost per MB charged to a WHMCS user at the end of every 24H period.",
             "Default" => "0.0",
         ],
     ];
